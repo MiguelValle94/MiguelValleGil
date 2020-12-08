1af6cd6b373db2d08c20f2cc2533bdad6d8c2ea2
miguelbachi5a <miguelbachi5a@gmail.com>
2020-12-08 18:41:26 +0100

    feat: use React snippets

---
+++ src/components/Generic/GenericButton.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class GenericButton extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default GenericButton
